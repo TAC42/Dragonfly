9b9b604e8818a6e7a9f46477db36aa41dae3e817
TAC42 <tomi.carlin@gmail.com>
2023-12-20 12:59:43 +0200

    initial-commit for dragonfly :)

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <i v-html="svg"></i>
+</template>
+
+<script>
+import { svgService } from '../services/svg.service.js'
+
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconName: String,
+  },
+  data() {
+    return {
+      svg: '',
+    }
+  },
+  mounted() {
+    this.svg = svgService.getIcon(this.iconName)
+  },
+}
+</script>
